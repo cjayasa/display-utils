61d1e2ef4b6f56688cb65dc94de80a005bbe6649
cjayasa <cjayget@gmail.com>
2024-02-13 18:50:30 +0100

    added tray icon functionality

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import pystray
 import pywintypes
 import win32api
 import win32con
+from PIL import Image
+from pystray import MenuItem as item
 
 devmode = pywintypes.DEVMODEType()
 devmode.PelsWidth = 1920
@@ -9,3 +12,29 @@ devmode.PelsHeight = 1200
 devmode.Fields = win32con.DM_PELSWIDTH | win32con.DM_PELSHEIGHT
 
 win32api.ChangeDisplaySettings(devmode, 0)
+
+
+def on_set_resolution(width: int, height: int):
+    devmode = pywintypes.DEVMODEType()
+    devmode.PelsWidth = width
+    devmode.PelsHeight = height
+
+    devmode.Fields = win32con.DM_PELSWIDTH | win32con.DM_PELSHEIGHT
+
+    win32api.ChangeDisplaySettings(devmode, 0)
+
+def on_quit():
+    icon.visible = False
+    icon.stop()
+
+if __name__ == '__main__':
+    image = Image.open("resolution.png")
+    
+    menu=(
+        item("16:10", lambda: on_set_resolution(1920, 1200)),
+        item("4:3", lambda: on_set_resolution(1600, 1200)),
+        item("Quit", on_quit)
+        )
+
+    icon = pystray.Icon("Res Switcher", image, "Res Switcher", menu)
+    icon.run()
